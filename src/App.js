7ed23640eabf2920d55e7384239675a3819c7f99
david <daverohrer@gmail.com>
2021-03-14 17:58:24 -0500

    cycles through the flashcard deck

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Flashcard from "./Flashcard";
 import { useState } from "react";
-import images from "./images/index";
+import images from "./images";
 
 function App() {
-  const [currentIndex, setCurrentindex] = useState();
-  //for now array of characters can go here
-  //also logic for switching between goes here
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleClick = () => {
+    setCurrentIndex((prevCurrentIndex) =>
+      prevCurrentIndex >= Object.keys(images).length - 1
+        ? 0
+        : prevCurrentIndex + 1
+    );
+  };
+
   const prompts = [
     { leftPrompt: "A", rightPrompt: "a", image: images.apple },
     { leftPrompt: "B", rightPrompt: "b", image: images.ball },
@@ -15,8 +21,7 @@ function App() {
     { leftPrompt: "D", rightPrompt: "d", image: images.dog },
   ];
 
-  //presentation/container components will be useful
-  return <Flashcard prompts={prompts[0]} />;
+  return <Flashcard prompts={prompts[currentIndex]} onClick={handleClick} />;
 }
 
 export default App;
