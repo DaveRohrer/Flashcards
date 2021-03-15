8b34329c81f23e211afe6c544bb6eb9e4233074e
david <daverohrer@gmail.com>
2021-03-15 10:50:34 -0500

    slighlty better animation. still learning

--- src/Flashcard.jsx
+++ src/Flashcard.jsx
@@ -15,7 +15,6 @@ function Flashcard(props) {
 
   const handleClick = () => {
     props.onClick();
-    setAnimationSwitch((prevAnimationSwitch) => !prevAnimationSwitch);
   };
 
   const [animationSwitch, setAnimationSwitch] = useState(true);
@@ -32,8 +31,8 @@ function Flashcard(props) {
         opacity: 1,
         transform: x
           .interpolate({
-            range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
-            output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1],
+            range: [0, 0.15, 0.85, 1],
+            output: [1, 0.95, 0.95, 1],
           })
           .interpolate((x) => `scale(${x})`),
       }}
