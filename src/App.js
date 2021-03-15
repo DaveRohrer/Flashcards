bb75b96d5373dd0cc8e539b50abbe53d0099d180
david <daverohrer@gmail.com>
2021-03-14 23:11:06 -0500

    Moved  card animation to presention component

--- src/App.js
+++ src/App.js
@@ -11,9 +11,6 @@ function App() {
     setCurrentIndex(
       (prevCurrentIndex) => (prevCurrentIndex + 1) % Object.keys(images).length
     );
-    setAnimationSwitch(
-      (prevAnimationSwitch) => prevAnimationSwitch == animationSwitch
-    );
   };
 
   const prompts = [
@@ -23,29 +20,7 @@ function App() {
     { leftPrompt: "D", rightPrompt: "d", image: images.dog },
   ];
 
-  const [animationSwitch, setAnimationSwitch] = useState(true);
-  const { x } = useSpring({
-    from: { x: 0 },
-    x: animationSwitch ? 1 : 0,
-    config: { duration: 1000 },
-  });
-
-  return (
-    <animated.div
-      onClick={() => setAnimationSwitch(!animationSwitch)}
-      style={{
-        opacity: 1,
-        transform: x
-          .interpolate({
-            range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
-            output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1],
-          })
-          .interpolate((x) => `scale(${x})`),
-      }}
-    >
-      <Flashcard prompts={prompts[currentIndex]} onClick={handleClick} />
-    </animated.div>
-  );
+  return <Flashcard prompts={prompts[currentIndex]} onClick={handleClick} />;
 }
 
 export default App;
--- src/Flashcard.jsx
+++ src/Flashcard.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
+import { useSpring, animated } from "react-spring";
 import "./index.css";
 
 function Flashcard(props) {
@@ -12,22 +13,47 @@ function Flashcard(props) {
     );
   });
 
+  const handleClick = () => {
+    props.onClick();
+    setAnimationSwitch((prevAnimationSwitch) => !prevAnimationSwitch);
+  };
+
+  const [animationSwitch, setAnimationSwitch] = useState(true);
+  const { x } = useSpring({
+    from: { x: 0 },
+    x: animationSwitch ? 1 : 0,
+    config: { duration: 1000 },
+  });
+
   return (
-    <div className="Flashcard" ref={FlashcardRef} onClick={props.onClick}>
-      <div className="FlashcardTextContainer" ref={TextContainerRef}>
-        <div className="FlashcardText">{props.prompts.leftPrompt}</div>
-        <div className="FlashcardText AlignRight">
-          {props.prompts.rightPrompt}
+    <animated.div
+      onClick={() => setAnimationSwitch(!animationSwitch)}
+      style={{
+        opacity: 1,
+        transform: x
+          .interpolate({
+            range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+            output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1],
+          })
+          .interpolate((x) => `scale(${x})`),
+      }}
+    >
+      <div className="Flashcard" ref={FlashcardRef} onClick={handleClick}>
+        <div className="FlashcardTextContainer" ref={TextContainerRef}>
+          <div className="FlashcardText">{props.prompts.leftPrompt}</div>
+          <div className="FlashcardText AlignRight">
+            {props.prompts.rightPrompt}
+          </div>
+        </div>
+        <div className="FlashcardImageContainer">
+          <img
+            src={props.prompts.image}
+            className="FlashcardImage"
+            height={verticalSpaceForImg}
+          />
         </div>
       </div>
-      <div className="FlashcardImageContainer">
-        <img
-          src={props.prompts.image}
-          className="FlashcardImage"
-          height={verticalSpaceForImg}
-        />
-      </div>
-    </div>
+    </animated.div>
   );
 }
 
